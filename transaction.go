b4730bb0b6d49cf24ff100862b0a578ff4540eeb
Gaurav Savanur <gauravsavanur07@gmail.com>
2018-07-11 14:18:25 +0530

    Fill Coinbase transaction data with random data

--- transaction.go
+++ transaction.go
@@ -69,7 +69,13 @@ func (out *TXOutput) CanBeUnlockedWith(unlockingData string) bool {
 // NewCoinbaseTX creates a new coinbase transaction
 func NewCoinbaseTX(to, data string) *Transaction {
 	if data == "" {
-		data = fmt.Sprintf("Reward to '%s'", to)
+	randData := make([]byte, 20)
+		_, err := rand.Read(randData)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		data = fmt.Sprintf("%x", randData)
 	}
 
 	txin := TXInput{[]byte{}, -1, data}
