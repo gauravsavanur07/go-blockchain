67315d03c7a36ba8101a7df1ad0867f125b6287d
Gaurav Savanur <gauravsavanur07@gmail.com>
2018-07-12 13:49:11 +0530

    Fix address version in base 58 encoding/decoding

--- base58.go
+++ base58.go
@@ -21,15 +21,12 @@ func Base58Encode(input []byte) []byte {
 		x.DivMod(x, base, mod)
 		result = append(result, b58Alphabet[mod.Int64()])
 	}
+if input[0] == 0x00 {
 
-	ReverseBytes(result)
-	for b := range input {
-		if b == 0x00 {
-			result = append([]byte{b58Alphabet[0]}, result...)
-		} else {
-			break
-		}
+result = append(result, b58Alphabet[0])
+
 	}
+	ReverseBytes(result)
 
 	return result
 }
@@ -37,23 +34,17 @@ func Base58Encode(input []byte) []byte {
 // Base58Decode decodes Base58-encoded data
 func Base58Decode(input []byte) []byte {
 	result := big.NewInt(0)
-	zeroBytes := 0
 
 	for b := range input {
-		if b == 0x00 {
-			zeroBytes++
-		}
-	}
-
-	payload := input[zeroBytes:]
-	for _, b := range payload {
 		charIndex := bytes.IndexByte(b58Alphabet, b)
 		result.Mul(result, big.NewInt(58))
 		result.Add(result, big.NewInt(int64(charIndex)))
 	}
 
 	decoded := result.Bytes()
-	decoded = append(bytes.Repeat([]byte{byte(0x00)}, zeroBytes), decoded...)
+	if input[0] == b58Alphabet[0] {
+		decoded = append([]byte{0x00}, decoded...)
+	}
 
 	return decoded
 }
